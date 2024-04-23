46a1b5e7d8177c37c654be77e787f03ba8d79494
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-04-23 10:24:27 +0200

    Fix some bugs

--- server/index.ts
+++ server/index.ts
@@ -1,20 +1,46 @@
 import express from 'express'
+import cors from 'cors';
 import recipesRoute from './recipes/routes/recipeRoutes.js'
 import ingredientsRoute from './ingredients/routes/ingredientRoutes.js'
 
 const app = express()
-const port = 4200
+
+// Utilisez le middleware cors pour autoriser les requêtes cross-origin
+app.use(cors());
+
+// Ajoutez le middleware express.json() pour analyser les corps de requête en JSON
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
 app.get('/api', (req, res) => {
   res.send('Hello World!')
-})
+});
 
-app.use('/api/recipes', recipesRoute)
-app.use('/api/ingredients', ingredientsRoute)
+app.use('/api/recipes', recipesRoute);
+app.use('/api/ingredients', ingredientsRoute);
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+app.listen(4200, () => {
+  console.log(`App listening on port 4200`)
+});
+
+// Logging server errors
+process.on('uncaughtException', function (err) {
+  console.log(err);
+});
+
+process.on('unhandledRejection', function (err) {
+  console.log(err);
+});
+
+process.on('SIGINT', function () {
+  console.log("Caught interrupt signal");
+  process.exit();
+});
+
+process.on('SIGTERM', function () {
+  console.log("Caught interrupt signal");
+  process.exit();
+});
