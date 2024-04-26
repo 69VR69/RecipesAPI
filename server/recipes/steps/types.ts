da387048b004a9eb41053c42c4a7dabec8c9aad8
zeyzen <64972509+zeyzen@users.noreply.github.com>
2024-04-26 16:50:00 +0200

    start steps API

---
+++ server/recipes/steps/types.ts
@@ -0,0 +1,11 @@
+// import { Prisma } from "@prisma/client";
+
+// // Step with ingredients
+// const Steps = Prisma.validator<Prisma.StepsDefaultArgs>()({
+//   })
+
+
+// export type Steps = Prisma.StepGetPayload<typeof Steps>;
+
+// // ingredients without id
+// export type StepsWithoutId = Omit<Steps, "id">; 
