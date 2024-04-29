09e33811fd42f94ee2798850fb0af164b2d1acd2
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-04-29 13:51:42 +0200

    Update package.json and RecipeManage.tsx, add dotenv dependency and update recipeId handling

--- client/src/components/StepDisplayer.tsx
+++ client/src/components/StepDisplayer.tsx
@@ -1,36 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
-import { Ingredient, IngredientQuantity, Season } from "../models/Ingredient";
-import { Step } from "../models/Step";
 import { StepCard } from "./Card";
+import { SERVER_ADDRESS } from "../CONSTANTS.js";
+import { Step } from "../models/Step.js";
 
-export function StepDisplayer() {
+export function StepDisplayer({ recipeId }: { recipeId: number }) {
 
-    async function getSteps() {
-        const response = await fetch('http://localhost:3001/step');
+    async function getSteps(): Promise<Step[]> {
+        const response = await fetch(SERVER_ADDRESS + '/recipes/' + recipeId + '/steps');
         return await response.json();
     }
 
     const { isLoading, error, data } = useQuery({ queryKey: ["steps"], queryFn: getSteps });
 
-    //if (isLoading) return <div>Loading...</div>;
+    if (isLoading) return <div>Loading...</div>;
 
-    //if (error) return <div>Error: {error.name + " : " + error.message}</div>;
-
-    // FOR TESTING PURPOSES ONLY
-    const ingredient1 = new Ingredient(1, "Tomato", "Vegetable", Season.SUMMER);
-    const ingredient2 = new Ingredient(2, "Potato", "Vegetable", Season.WINTER);
-    const ingredientQuantity1 = new IngredientQuantity(ingredient1, 2);
-    const ingredientQuantity2 = new IngredientQuantity(ingredient2, 3);
-    const dataTemp = [new Step(1, "Step 1", [ingredientQuantity1]),
-    new Step(2, "Step 2", [ingredientQuantity2]),
-    new Step(3, "Step 3", [ingredientQuantity1, ingredientQuantity2]),
-    new Step(4, "Step 4", [ingredientQuantity1])];
+    if (error) return <div>Error: {error.name + " : " + error.message}</div>;
 
     return (
         <div>
             <h2>Steps</h2>
             <ul>
-                {dataTemp.map((step, index) => (
+                {data && data.map((step, index) => (
                     <StepCard key={index} description={step.description} ingredients={step.ingredients} />
                 ))}
             </ul>
