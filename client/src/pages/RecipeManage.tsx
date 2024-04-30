3af00819aca958a32ca9fd0d85eed8d95a9e8d4f
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-04-30 20:17:41 +0200

    Refactor schema.prisma and seed.ts, and update RecipeManage.tsx

--- client/src/pages/RecipeManage.tsx
+++ client/src/pages/RecipeManage.tsx
@@ -1,6 +1,9 @@
-import { useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
 import { IngredientDisplayer } from "../components/IngredientDisplayer";
 import { StepDisplayer } from "../components/StepDisplayer";
+import { SERVER_ADDRESS } from "../CONSTANTS";
+import { Recipe } from "../models/Recipe";
 
 
 export function RecipeManage()
@@ -8,9 +11,22 @@ export function RecipeManage()
     const { recipeId } = useParams();
     const parsedRecipeId = recipeId ? parseInt(recipeId) : 0;
 
+    async function fetchRecipes()
+    {
+        return fetch(SERVER_ADDRESS + '/recipes/' + parsedRecipeId)
+        .then(res => res.json())
+        .then(data => data as Recipe);
+    }
+
+    const { isLoading, error, data } = useQuery({ queryKey: ["recipes"], queryFn: fetchRecipes });
+
+    if (isLoading) return <div>Loading...</div>;
+
+    if (error) return <div>Error: {error.name + " : " + error.message + "\n" + error.stack}</div>;
+
     return (
         <div>
-            <h1>Recipes Manage of {parsedRecipeId}</h1>
+            <h1>Recipes of {data ? data.name : 'recipe number ' + parsedRecipeId}</h1>
             <IngredientDisplayer recipeId={parsedRecipeId} />
             <StepDisplayer recipeId={parsedRecipeId}/>
         </div>
--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,4 +1,5 @@
 import { createSeedClient } from "@snaplet/seed";
+import { copycat } from "@snaplet/copycat";
 
 const main = async () => {
 
@@ -19,9 +20,19 @@ const main = async () => {
 
     await seed.ingredient((x) => x(35));
     await seed.recipe((x) => x(5));
-    await seed.recipeIngredient((x) => x(23));
+    await seed.recipeIngredient((x) => x(23,
+        ({ seed }) => ({
+            // Set the number of ingredients  to a value between 1 and 15
+            number: copycat.int(seed, { min: 1, max: 15 })
+        })
+    ));
     await seed.step((x) => x(30));
-    await seed.stepIngredient((x) => x(28));
+    await seed.stepIngredient((x) => x(28,
+        ({ seed }) => ({
+            // Set the number of ingredients  to a value between 1 and 15
+            number: copycat.int(seed, { min: 1, max: 15 })
+        })
+    ));
 
     console.log("Seeding completed")
 
