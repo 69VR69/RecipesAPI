442fdb22252d487fb3afd2c956f7f874ad6cfbf4
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-04-30 14:10:25 +0200

    Add Step and StepIngredient seeding in seed.ts

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -21,6 +21,8 @@ const main = async () => {
     await seed.Ingredient((x) => x(35), { connect: true });
     await seed.Recipe((x) => x(5), { connect: true });
     await seed.RecipeIngredient((x) => x(23), { connect: true });
+    await seed.Step((x) => x(30), { connect: true });
+    await seed.StepIngredient((x) => x(28), { connect: true });
 
     console.log("Seeding completed")
 
